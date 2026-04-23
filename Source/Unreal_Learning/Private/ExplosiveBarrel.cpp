adb8be9ac4b5492ea5d9a57cc74e188062bb74db
DaleBruise <DaleBruise@outlook.com>
2026-04-23 20:29:06 +0800

    update
    1. Add Cross hair

--- Source/Unreal_Learning/Private/ExplosiveBarrel.cpp
+++ Source/Unreal_Learning/Private/ExplosiveBarrel.cpp
@@ -2,6 +2,8 @@
 
 
 #include "ExplosiveBarrel.h"
+
+#include "DrawDebugHelpers.h"
 #include "Components/StaticMeshComponent.h"
 #include "PhysicsEngine/RadialForceComponent.h"
 
@@ -48,4 +50,22 @@ void AExplosiveBarrel::OnActorHit(UPrimitiveComponent* HitComponent,
 	FVector NormalImpulse, const FHitResult& Hit)
 {
 	this->_force_comp->FireImpulse();
+	
+	UE_LOG(LogTemp, Log, 
+		TEXT("OnActorHit in Explosive Barrel"));
+	
+	UE_LOG(LogTemp, Warning, 
+		TEXT("OtherActor: %s, at game time: %f"), 
+		*GetNameSafe(OtherActor),
+		GetWorld()->TimeSeconds);
+	
+	FString CombinedString = FString::Printf(TEXT("Hit at location: %s"), 
+		*Hit.ImpactPoint.ToString());
+	DrawDebugString(GetWorld(), 
+		Hit.ImpactPoint, 
+		CombinedString, 
+		nullptr, 
+		FColor::Green, 
+		2.0f, 
+		true);
 }
