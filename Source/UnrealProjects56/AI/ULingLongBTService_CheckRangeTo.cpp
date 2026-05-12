a3799c78a2dcbbe9aadf5f725e504b701d0c0576
DaleBruise <DaleBruise@outlook.com>
2026-05-12 16:30:52 +0800

    Update Commit
    1. Add BT Services

---
+++ Source/UnrealProjects56/AI/ULingLongBTService_CheckRangeTo.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ULingLongBTService_CheckRangeTo.h"
+#include "AIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+void UULingLongBTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp,
+                                                uint8* NodeMemory,
+                                                float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	const auto Ptr = OwnerComp.GetBlackboardComponent();
+	check(Ptr)
+	auto* BBComp = Ptr->GetValueAsObject(this->TargetActorKey.SelectedKeyName);
+	check(BBComp)
+	const auto TargetActor = Cast<AActor>(BBComp);
+
+	const FVector TargetLocation = TargetActor->GetActorLocation();
+	auto Controller = OwnerComp.GetAIOwner();
+	if (Controller != nullptr)
+	{
+		APawn* OwningPawn = Controller->GetPawn();
+		check(OwningPawn)
+		FVector OriginLocation = OwningPawn->GetActorLocation();
+
+		float DistanceTo = FVector::Dist(
+			TargetLocation,
+			OriginLocation);
+		bool IfWithinRange = DistanceTo < this->MaxAttackRange;
+aa
+		bool IfHasLOS = Controller->LineOfSightTo(TargetActor);
+		
+		Ptr->SetValueAsBool(this->WithinRangeKey.SelectedKeyName,
+		                    IfWithinRange && IfHasLOS);
+	}
+}
---
+++ Source/UnrealProjects56/AI/ULingLongBTService_CheckRangeTo.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "ULingLongBTService_CheckRangeTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECTS56_API UULingLongBTService_CheckRangeTo : public UBTService
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditAnywhere, Category = "AI")
+	FBlackboardKeySelector TargetActorKey;
+	
+	UPROPERTY(EditAnywhere, Category = "AI")
+	FBlackboardKeySelector WithinRangeKey;
+	
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float MaxAttackRange = 500.0f;
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp,
+	                      uint8* NodeMemory,
+	                      float DeltaSeconds) override;
+};
